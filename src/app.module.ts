d90a4eaf7548c2ad5a7bd5a2ccf7a80e4c72e91d
Jangseungup <jsu917@naver.com>
2021-08-25 10:40:24 +0900

    nestjs backend update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ContactsController } from './contacts/contacts.controller';
+import { ContactsModule } from './contacts/contacts.module';
+import { ContactsService } from './contacts/contacts.service';
+import { PrismaService } from './contacts/prisma.service';
 
 @Module({
-  imports: [],
+  imports: [ContactsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
